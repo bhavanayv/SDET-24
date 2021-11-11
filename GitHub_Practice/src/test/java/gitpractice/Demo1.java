f98b0a950e55d447e16f2d8063ff50bf24ee6532
Admin <Admin@DESKTOP-4FJ4BMF>
2021-11-11 12:18:29 +0530

    SDET-24 repo

---
+++ GitHub_Practice/src/test/java/gitpractice/Demo1.java
@@ -0,0 +1,11 @@
+package gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void testDemo1()
+	{
+		System.out.println("this is the first time im using commit");
+	}
+}
