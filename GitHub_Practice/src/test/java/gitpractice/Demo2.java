912235237f85e6522abf99648abc4b26179fe9f1
Admin <Admin@DESKTOP-4FJ4BMF>
2021-11-11 15:33:53 +0530

    user 1 conflict demo

---
+++ GitHub_Practice/src/test/java/gitpractice/Demo2.java
@@ -0,0 +1,16 @@
+package gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	public class testDemo2 {
+		@Test
+		public void testDemo1()
+		{
+			System.out.println("HELLO, This is the first time user 1 is using commit");
+			System.out.println("haiiii");
+			
+			System.out.println("user 1 conflict");
+		}
+	}
+}
