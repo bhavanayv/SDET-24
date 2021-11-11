dc330ff4d2475107ba3061056e48d4bad0ce6c6c
Admin <Admin@DESKTOP-4FJ4BMF>
2021-11-11 12:38:35 +0530

    This is user 1 commit

--- GitHub_Practice/src/test/java/gitpractice/Demo1.java
+++ GitHub_Practice/src/test/java/gitpractice/Demo1.java
@@ -6,6 +6,6 @@ public class Demo1 {
 	@Test
 	public void testDemo1()
 	{
-		System.out.println("this is the first time im using commit");
+		System.out.println("HELLO, This is the first time user 1 is using commit");
 	}
 }
